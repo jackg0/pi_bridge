1914857df4b2feaa5a25397c8b6d656b507e8226
jackg0 <jackg7@vt.edu>
2017-04-01 17:18:45 -0400

    changed pi_bridge script to match new package, service name

--- scripts/pi_bridge.py
+++ scripts/pi_bridge.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-NAME = 'add_two_ints_server'
+NAME = 'pi_bridge_server'
 
-# import the AddTwoInts service
-from bridge_practice.srv import *
+# import the PiBridge service
+from pi_bridge.srv import *
 import rospy
 
-def add_two_ints(req):
+def pi_bridge(req):
 	print("Returning [x: %s, y: %s, z: %s]" % (req.a, req.b, req.c))
-	AddTwoIntsResponse(rospy.Time.now())
+	PiBridgeResponse(rospy.Time.now())
 
-def add_two_ints_server():
+def pi_bridge_server():
 	rospy.init_node(NAME)
-	s = rospy.Service('add_two_ints', AddTwoInts, add_two_ints)
+	s = rospy.Service('pi_bridge', PiBridge, pi_bridge)
 
 	rospy.spin()
 
 if __name__ == "__main__":
-	add_two_ints_server()
+	pi_bridge_server()
 
